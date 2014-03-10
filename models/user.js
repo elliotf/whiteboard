3f109d0309b742cdcee26963921cf1fb60adeaa0
Elliot Foster <efoster@firetaco.com>
2014-03-09 22:52:27 -0700

    User.findOrCreateFromOAUTH for passport

--- models/user.js
+++ models/user.js
@@ -1,9 +1,29 @@
-var db = require('./base')
+var db      = require('./base')
+  , _       = require('lodash')
   , User
 ;
 
 User = db.Model.extend({
   tableName: 'users'
+}, {
+  findOrCreateFromOAUTH: function(input, cb) {
+    var data = {
+      oauth_provider: input.provider
+      , oauth_id:     input.id
+      , email:        input.emails[0].value
+    }
+    var user = User.forge(data);
+
+    user
+      .fetch()
+      .then(function(found){
+        if (found) {
+          return cb(null, found);
+        }
+
+        user.save().exec(cb);
+      })
+  }
 });
 
 module.exports = {
--- test/models/user.js
+++ test/models/user.js
@@ -6,11 +6,11 @@ var helper = require('../../support/spec_helper')
 
 describe("User model", function() {
   it("can be instantiated", function() {
-    var user = new User();
+    var user = User.forge();
   });
 
   it("can be saved", function(done) {
-    var user = new User({
+    var user = User.forge({
       email: 'e@example.com'
       , oauth_provider: 'firetaco oauth'
       , oauth_id: 'an oauth id'
@@ -29,6 +29,56 @@ describe("User model", function() {
       });
 
       done();
+    });
+  });
+
+  describe(".findOrCreateFromOAUTH", function() {
+    beforeEach(function() {
+      this.oauth_data = {
+        emails:        [
+          { value: 'focfo@example.com' }
+        ]
+        , provider:    'firetaco oauth'
+        , id:          'an oauth id'
+        , displayName: 'john doe'
+      };
+    });
+
+    context("When the user exists", function() {
+      beforeEach(function(done) {
+        User.forge({
+          email:            'focfo@example.com'
+          , oauth_provider: 'firetaco oauth'
+          , oauth_id:       'an oauth id'
+        }).save().exec(done);
+      });
+
+      it("Returns the existing user", function(done) {
+        this.sinon.spy(User.prototype, 'save');
+
+        User
+          .findOrCreateFromOAUTH(this.oauth_data, function(err, user){
+            expect(err).to.not.exist;
+            expect(User.prototype.save).to.not.have.been.called;
+            expect(user.get('email')).to.equal('focfo@example.com');
+            done();
+          })
+      });
+    });
+
+    context("When the user does not exist", function() {
+      it("Creates the user", function(done) {
+        this.sinon.spy(User.prototype, 'save');
+
+        User
+          .findOrCreateFromOAUTH(this.oauth_data, function(err, user){
+            expect(err).to.not.exist;
+
+            expect(User.prototype.save).to.have.been.called;
+            expect(user.get('email')).to.equal('focfo@example.com');
+            done();
+          });
+      });
     });
   });
 
