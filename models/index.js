a7df97fca6148bac2ba405f5b757d2d81955b38c
Elliot Foster <efoster@firetaco.com>
2014-03-15 15:52:08 -0700

    Ensure I don't try to alphabetize this again
    Circular dependencies for the loss, unfortunately.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   db:     require('./base')
-  , User: require('./user').User
+  , User: require('./user').User // must be loaded before Page
   , Page: require('./page').Page
 };
